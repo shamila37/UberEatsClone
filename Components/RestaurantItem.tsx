b2cb695d6fe17f50aed79ac0f432eefc0db3f6b7
shamila37 <shamilaashan37@gmail.com>
2024-04-04 07:56:10 +0530

    update to the .tsx

--- Components/RestaurantItem.tsx
+++ Components/RestaurantItem.tsx
@@ -1,5 +1,5 @@
 import { View, Text, Image, TouchableOpacity } from 'react-native'
-import React from 'react'
+import React, { ReactNode } from 'react'
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 import Categories from './Categories';
 
@@ -16,21 +16,36 @@ const localRestaurents = [
     },
     {
         name: "Benihana",
-        image_url: "https://wallpapers.com/images/featured/restaurant-background-2ez77umko2vj5w02.jpg",
+        image_url: "https://wallpapers.com/images/high/el-farallon-restaurant-in-cabo-san-lucas-mexico-su6h4hr7d6blpemp.webp",
         categories: ["Cafe", "Bar"],
         price: "$$",
         reviews: 124,
         rating: 3.7,
     },
     {
-        name: "Be Bar",
-        image_url: "https://wallpapers.com/images/featured/restaurant-background-2ez77umko2vj5w02.jpg",
+        name: "India's Grill",
+        image_url: "https://wallpapers.com/images/high/pub-wood-aesthetic-photography-5mecgmn8o9yp6xb5.webp",
+        categories: ["Indian", "Bar"],
+        price: "$$",
+        reviews: 700,
+        rating: 4.9,
+    },
+    {
+        name: "Benihana",
+        image_url: "https://wallpapers.com/images/high/restaurant-background-fqxbzampnmnommt0.webp",
         categories: ["Cafe", "Bar"],
         price: "$$",
-        reviews: 12404,
-        rating: 4.5,
+        reviews: 124,
+        rating: 3.7,
     },
-    
+    {
+        name: "India's Grill",
+        image_url: "https://wallpapers.com/images/high/restaurant-background-66dqs6aozq74vmqn.webp",
+        categories: ["Indian", "Bar"],
+        price: "$$",
+        reviews: 700,
+        rating: 4.9,
+    },
 ];
 
 export default function RestaurantItem() {
@@ -46,11 +61,11 @@ export default function RestaurantItem() {
                 backgroundColor: "white"
                 }}>
               {/* restaurent image */}
-              <RestaurantImage image={localRestaurents[0].image_url}/>
+              <RestaurantImage image={restaurent.image_url}/>
               {/* restaurent info */}
               <RestaurantInfo 
-              name={localRestaurents[0].name} 
-              rating={localRestaurents[0].rating}
+              name={restaurent.name} 
+              rating={restaurent.rating}
               />
             </View>
         ))}
@@ -73,7 +88,9 @@ export default function RestaurantItem() {
     </>
     );
 
-    const RestaurantInfo = (props: { rating: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | null | undefined; }) => (
+    const RestaurantInfo = (props: {
+        [x: string]: ReactNode; rating: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | null | undefined; 
+}) => (
         <View style={{
             flexDirection: "row",
             justifyContent: "space-between", 
@@ -81,7 +98,7 @@ export default function RestaurantItem() {
             marginTop: 10,
             }}>
             <View>
-                <Text style={{fontSize: 15, fontWeight: "bold"}}>props.name</Text>
+                <Text style={{fontSize: 15, fontWeight: "bold"}}>{props.name}</Text>
                 <Text style={{fontSize: 13, color: "gray"}}>30-45 . min</Text>
             </View>
             <View style={{
